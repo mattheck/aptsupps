d93ddd0287e121b691e69838aa8f4005bc020d94
mattheck <matt.heckmann@gmail.com>
2015-11-23 12:58:32 -0500

    scratch 1pm 11/23

--- scratch.php
+++ scratch.php
@@ -1,114 +1,15 @@
-<?php 
+<h2>WHAT TO EXPECT WHEN USING SUPERSET™</h2>
+<p style="margin-bottom: 10px !important;">Performance! Minutes after ingesting your first dose of SUPERSET™ you will feel “it” kick in.* You’re not going to grow a second set of ‘traps’, but you can expect significant improvements in exercise performance when properly using SUPERSET™ in conjunction with a proper diet and exercise.</p>
+<p style="margin-bottom: 10px !important;">So You Can Expect:</p>
 
-add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles',50);
-function salient_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
+<ul class="expect-list">
+    <li>  To See Exactly What Is In Your Product, How Much Of It And No Mystery Blends*</li>
+    <li>  To Grow In Confidence When Training*</li>
+    <li>  To Increase In Strength, Weight And Size*</li>
+    <li>  To Achieve Personal Records “PR’s” In Major Lifts*</li>
+    <li>  To Experience A Much-Improved Training Experience*</li>
+    <li>  To Recover Faster From Exercise*</li>
+</ul>
 
-#-----------------------------------------------------------------#
-# Bottom menu
-#-----------------------------------------------------------------#
-if ( function_exists( 'register_nav_menus' ) ) {
-	register_nav_menus(
-		array(
-		  'bottom_nav' => 'Bottom Navigation Menu',
-		)
-	);
-}	
-
-
-
-#-----------------------------------------------------------------#
-# Exclude Category from Shop
-#-----------------------------------------------------------------#
-
-add_filter( 'get_terms', 'get_subcategory_terms', 10, 3 );
-
-function get_subcategory_terms( $terms, $taxonomies, $args ) {
-
-  $new_terms = array();
-
-  // if a product category and on the shop page
-  if ( in_array( 'product_cat', $taxonomies ) && ! is_admin() && is_shop() ) {
-
-    foreach ( $terms as $key => $term ) {
-
-      if ( ! in_array( $term->slug, array( 'joint-health', 'lean-gainer', 'multivitamin', 'muscle-building', 'nitric-oxide', 'pre-workout', 'protein', 'recovery', 'test-booster', 'thermogenic', 'weight-loss','merch') ) ) {
-        $new_terms[] = $term;
-      }
-
-    }
-
-    $terms = $new_terms;
-  }
-
-  return $terms;
-}
-
-function wpse_allowedtags() {
-    // Add custom tags to this string
-        return '<script>,<style>,<br>,<em>,<i>,<ul>,<ol>,<li>,<a>,<p>,<img>,<video>,<audio>'; 
-    }
-
-if ( ! function_exists( 'wpse_custom_wp_trim_excerpt' ) ) : 
-
-    function wpse_custom_wp_trim_excerpt($wpse_excerpt) {
-    $raw_excerpt = $wpse_excerpt;
-        if ( '' == $wpse_excerpt ) {
-
-            $wpse_excerpt = get_the_content('');
-            $wpse_excerpt = strip_shortcodes( $wpse_excerpt );
-            $wpse_excerpt = apply_filters('the_content', $wpse_excerpt);
-            $wpse_excerpt = str_replace(']]>', ']]&gt;', $wpse_excerpt);
-            $wpse_excerpt = strip_tags($wpse_excerpt, wpse_allowedtags()); /*IF you need to allow just certain tags. Delete if all tags are allowed */
-
-            //Set the excerpt word count and only break after sentence is complete.
-                $excerpt_word_count = 75;
-                $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
-                $tokens = array();
-                $excerptOutput = '';
-                $count = 0;
-
-                // Divide the string into tokens; HTML tags, or words, followed by any whitespace
-                preg_match_all('/(<[^>]+>|[^<>\s]+)\s*/u', $wpse_excerpt, $tokens);
-
-                foreach ($tokens[0] as $token) { 
-
-                    if ($count >= $excerpt_length && preg_match('/[\,\;\?\.\!]\s*$/uS', $token)) { 
-                    // Limit reached, continue until , ; ? . or ! occur at the end
-                        $excerptOutput .= trim($token);
-                        break;
-                    }
-
-                    // Add words to complete sentence
-                    $count++;
-
-                    // Append what's left of the token
-                    $excerptOutput .= $token;
-                }
-
-            $wpse_excerpt = trim(force_balance_tags($excerptOutput));
-
-                $excerpt_end = ' <a href="'. esc_url( get_permalink() ) . '">' . '&nbsp;&raquo;&nbsp;' . sprintf(__( 'Read more about: %s &nbsp;&raquo;', 'wpse' ), get_the_title()) . '</a>'; 
-                $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end); 
-
-                $pos = strrpos($wpse_excerpt, '</');
-                if ($pos !== false)
-                // Inside last HTML tag
-                $wpse_excerpt = substr_replace($wpse_excerpt, $excerpt_end, $pos, 0); /* Add read more next to last word */
-                else
-                // After the content
-                $wpse_excerpt .= $excerpt_more; /*Add read more in new paragraph */
-
-            return $wpse_excerpt;   
-
-        }
-        return apply_filters('wpse_custom_wp_trim_excerpt', $wpse_excerpt, $raw_excerpt);
-    }
-
-endif; 
-
-remove_filter('get_the_excerpt', 'wp_trim_excerpt');
-add_filter('get_the_excerpt', 'wpse_custom_wp_trim_excerpt'); 
-
-?>
+<h2>So why use SUPERSET™?</h2>
+<p style="margin-bottom: 52px !important;">Isn’t it true that regardless of where and how you train the reason you do train is to get results? If you agree with that statement you’ll appreciate the next: APT SPORTS® did not want to create a $35.00 buzz! What SUPERSET™ IS NOT is overstimulated with excessive doses of untested and dangerous ingredients that may be banned in a year. Regretfully, many products in the industry are too focused on the buzz element and not performance.</p>
